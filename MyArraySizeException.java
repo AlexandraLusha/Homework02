3e6d3aa64f065837824f12e039835abf8d63e741
AlexandraLusha <lusha82000@mail.ru>
2021-04-25 20:52:05 +0300

    Add files via upload

---
+++ MyArraySizeException.java
@@ -0,0 +1,8 @@
+package Java_Core.Lesson02;
+
+public class MyArraySizeException extends Exception {
+        //    MyArraySizeException(String msg) {
+        //        super(msg);
+        //    }
+
+}
